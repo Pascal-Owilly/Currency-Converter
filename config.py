0f89b019630121bd22e3ca5b6a4a80faba2ec0ed
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-03-16 18:15:38 +0300

    add our apps configurations

---
+++ config.py
@@ -0,0 +1,39 @@
+
+import os
+
+class Config:
+   
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    API_KEY = 'ENTER-YOUR-API-KEY'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/currency_c'
+    #email configurations
+    MAIL_SERVER = 'smtp.google.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+   
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+   
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:silvano36@localhost/blog_site' 
+    
+    DEBUG = True
+
+class TestConfig(Config):
+    
+    DEBUG = True
+
+config_options = {
+  'development':DevConfig,
+  'production':ProdConfig,
+  'test':TestConfig  
+}
